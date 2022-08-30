4678eca7003d061ec2a60d5eaa1f024fa258a4b6
andyadc <andaicheng@gmail.com>
2022-08-30 09:51:42 +0800

    api gateway

--- api-gateway-core-02/src/test/java/com/andyadc/gateway/test/APITests.java
+++ api-gateway-core-02/src/test/java/com/andyadc/gateway/test/APITests.java
@@ -1,14 +1,30 @@
 package com.andyadc.gateway.test;
 
+import com.andyadc.gateway.session.Configuration;
+import com.andyadc.gateway.session.GenericReferenceSessionFactoryBuilder;
+import io.netty.channel.Channel;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Future;
 
 public class APITests {
 
     private final Logger logger = LoggerFactory.getLogger(APITests.class);
 
     @Test
-    public void test_02() throws Exception {
+    public void test_GenericReference() throws Exception {
+        Configuration configuration = new Configuration();
+        configuration.addGenericReference("api-gateway-test",
+                "cn.bugstack.gateway.rpc.IActivityBooth",
+                "sayHi");
+
+        GenericReferenceSessionFactoryBuilder builder = new GenericReferenceSessionFactoryBuilder();
+        Future<Channel> future = builder.build(configuration);
+
+        logger.info("服务启动完成 {}", future.get().id());
+
+        Thread.sleep(Long.MAX_VALUE);
     }
 }
