4c0d979cbca47122e977ac9713b004329fcf885a
andyadc <andaicheng@gmail.com>
2022-09-01 17:24:18 +0800

    api gateway

---
+++ api-gateway-core-03/src/main/java/com/andyadc/gateway/session/GatewaySession.java
@@ -0,0 +1,14 @@
+package com.andyadc.gateway.session;
+
+import com.andyadc.gateway.bind.IGenericReference;
+
+/**
+ * 用户处理网关 HTTP 请求
+ */
+public interface GatewaySession {
+    Object get(String uri, Object parameter);
+
+    IGenericReference getMapper(String uri);
+
+    Configuration getConfiguration();
+}
